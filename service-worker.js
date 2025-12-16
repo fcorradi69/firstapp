d178ac9fe2c5432ed3d4318a3f6db5969590361c
fcorradi69 <82023644+fcorradi69@users.noreply.github.com>
2025-12-16 12:40:29 +0000

    update

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'simple-pwa-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/service-worker.js',
+  '/manifest.webmanifest',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install: cache dei file base
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Activate: pulizia cache vecchie (qui semplice)
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch: rispondi dalla cache se possibile, altrimenti vai in rete
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
